e9f0f848440880424aa7bdfe023c315bd5655819
Grishma Ptael <patelgrishma9911@gmail.com>
2019-09-23 20:49:45 -0400

    made some basic structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers VALUES ('HamBurger');
+INSERT INTO burgers VALUES ('ChikenBurger');
+INSERT INTO burgers VALUES ('TurkeyBurger');
+INSERT INTO burgers VALUES ('VegBurger');
+INSERT INTO burgers VALUES ('PotatoPetiesBurger');
