5159859e99d9cf5b43c2d81fe6746aeca86db045
Grishma Ptael <patelgrishma9911@gmail.com>
2019-11-12 21:13:44 -0500

    all files

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+// create the connection information for the sql database
+var connection;
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'Grishu9911@',
+		database: 'burgers_db'
+	});
+};
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
+module.exports = connection;
